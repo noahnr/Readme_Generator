7c691f6be02bdbe4e6a52bd196bd4fd9c8113f95
noahnr <57819545+noahnr@users.noreply.github.com>
2020-06-17 21:13:34 -0500

    Commit with in line questions!

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,60 @@
+const inquirer = require("inquirer");
+
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Give a short description of the project, directions for installation, usage information, contribution guidlines, and instructions to test the project.",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Give short instructions to installing your application",
+        name: "installing"
+    },
+    {
+        type: "input",
+        message: "Give a short description of how to run a test on your app.",
+        name: "runTest"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license will you be using?",
+        choices: [
+            {name: 'GPL'},
+            {name: 'MIT'},
+            {name: 'Apache License 2.0'},
 
+        ],
+    },
+    {
+        type: "input",
+        message: "Author",
+        name: "author"
+    },
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "Github"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "Email"
+    },
 ];
+return inquirer.prompt(questions);
+
 
 // function to write README file
 function writeToFile(fileName, data) {
+ 
 }
 
 // function to initialize program
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,23 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  #Description
+  ${data.description}
+  #Table of Contents
+  
+  #Installing
+  ${data.installing}
+  #Usage
+  ${data.Usage}
+  #License
+  ${data.license}
+  #Contributing
+  ${data.author}
+  #Tests
+  ${data.runTest}
+  #Questions
+  ${data.email}
+  ${data.GitHub}
 `;
 }
 
